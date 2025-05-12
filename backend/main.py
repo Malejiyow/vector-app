761d82644106c89284e9e797e45b9ec04825a47a
Malejiyow <prydoxcompany@gmail.com>
2025-05-12 16:28:26 -0500

    python cambios

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI 
+from pydantic import  BaseModel
+
+app = FastAPI()
+
+class vectores(BaseModel):
+    Ax : float 
+    Ay : float 
+    Bx : float
+    By : float
+
+
+@app.post("/suma_vectores")
+def suma_vectores(vectores: vectores):
+    resultado_x = vectores.Ax + vectores.Bx
+    resultado_y = vectores.Ay + vectores.By
+    return {"Rx": resultado_x, "Ry": resultado_y}
